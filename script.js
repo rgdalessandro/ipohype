956f31e99882f7c895e802eb86ade17837a17113
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-07-07 13:41:53 -0500

    fixed loading page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var todayDate = new Date();
-    fetchMonth(todayDate.getMonth() + 1, todayDate.getFullYear(), function(msg) { // Call the function to show this month's IPOs
+    fetchMonth(8 + 1, 2015, function(msg) { // Call the function to show this month's IPOs
         goodDays = [];
 
         for (i in msg.results) {
